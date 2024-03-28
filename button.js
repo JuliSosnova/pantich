be6292b76846fcc13a746c02cf2f0d4faec2f469
DrScorp <146968499+JuliSosnova@users.noreply.github.com>
2024-03-28 19:47:19 +0800

    Add files via upload

--- button.js
+++ button.js
@@ -10,28 +10,36 @@ function FindResidual(){
         document.getElementById('result').value = 'Это не вектор функции (есть числа помимо 0 и 1)';
     }
     if(vector.length%2!=0 ){
-        document.getElementById('result').value = 'Это не вектор функции (длина не кратна 2)';
+        document.getElementById('result').value = 'Это не вектор функции (длина не степень 2)';
     }
     else{
         const form = document.getElementById("ost");
-        let T=form.elements["d"].value;
-        let el=document.getElementById('nom').value;
+        let T=Number(form.elements["d"].value);
+        let el=Number(document.getElementById('nom').value);
+        //alert(typeof T);
         if(el>vector.length){
             document.getElementById('result').value = 'Неверное значение номера аргумента';
         }
-        else{
+        else{ 
+            let v = [];
+            let k = vector.length, l = 0;
+            while (parseInt(k / 2) !== 0) {
+                k = parseInt(k / 2);
+                l++;
+                v.push(l);
+            }
+            
+            v.reverse();
+            el = v[el - 1];
             let r='';
             for(let i=0;i<vector.length;i++){
-               // alert(vector+" "+T+" "+el);
                 let p=i%(1 << el)
                 let d= 1<<(el-1);
-                if(p/d==T){
+                if(parseInt((p/d),10)==T){
                     r+=vector[i];
-                   //alert(r+" "+i);
                 }
-               // alert(d);
             }
-           // alert(r);
+            document.getElementById('result').value = r;
         }
     }
     
